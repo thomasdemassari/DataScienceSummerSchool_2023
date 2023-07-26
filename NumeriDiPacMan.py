8615a6dbc792fdc721b4f636194a73c2b018ff66
De Massari Thomas <thomas.demassari@icloud.com>
2023-07-26 16:43:51 +0200

    NumeriDiPacMan.py

---
+++ NumeriDiPacMan.py
@@ -0,0 +1,17 @@
+def numnum(num):
+    if num <= 20:
+        ris = num*num
+        return ris
+    else:
+        return num
+
+
+# NON TOCCARE, queste linee devono funzionare
+x = numnum(3)       # Deve RITORNARE 9
+print(x)
+
+y = numnum(10)      # Deve RITORNARE 100
+print(y)
+
+z = numnum(33)      # Deve RITORNARE 33
+print(z)
