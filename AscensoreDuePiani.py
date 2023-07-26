5ad72688d77d3e8748f7fb6ceb5d59c886bb05e0
De Massari Thomas <thomas.demassari@icloud.com>
2023-07-26 16:37:29 +0200

    Create AscensoreDuePiani.py

---
+++ AscensoreDuePiani.py
@@ -0,0 +1,9 @@
+piano_utente = 'terra'; piano_ascensore = 'rialzato'     # rialzato, terra, DING!
+piano_utente = 'rialzato'; piano_ascensore = 'terra'    # terra, rialzato, DING!
+piano_utente = 'terra'; piano_ascensore = 'terra'       # DING!
+piano_utente = 'rialzato'; piano_ascensore = 'rialzato' # DING!
+
+if (piano_ascensore != piano_utente):
+    print(piano_ascensore)
+    print(piano_utente)
+print("DING")
