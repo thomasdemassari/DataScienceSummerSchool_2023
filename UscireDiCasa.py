7664899756c8b536d96d9acc11e9ef2c33ca29a6
De Massari Thomas <thomas.demassari@icloud.com>
2023-07-26 16:35:52 +0200

    UscireDiCasa.py

--- UscireDiCasa.py
+++ UscireDiCasa.py
@@ -15,3 +15,20 @@ else:
                 print("Non ho fatto colazione e non ho lavato i denti \nNON posso uscire di casa")
             else:
                 print("Condizione non gestita")
+
+#Prova a fare un programma in Python che ti dice se puoi uscire di casa solo se hai fatto colazione (per cui devi avere almeno un tipo di latte) e lavato i denti
+ho_latte_intero = False
+ho_latte_scremato = False
+lavato_denti = False
+fatto_colazione = (ho_latte_intero) or (ho_latte_scremato)
+
+if (fatto_colazione == True) and (lavato_denti == True):
+        print("Hai fatto colazione e hai lavato i denti. \nPuoi uscire di casa")
+else:
+    if (fatto_colazione == True) and (lavato_denti == False):
+        print("Hai fatto colazione, ma non ti sei lavato i denti. \nNon puoi uscire di casa")
+    else:
+        if (lavato_denti == True) and (fatto_colazione == False):
+            print("Non hai fatto colazione perchè non hai il latte, ma hai lavato i denti. \nNon puoi uscire di casa")
+        else:
+             print("Non hai fatto colazione perchè non hai il latte e non ti sei neanche lavato i denti. \nLavati i denti e vai a comprare il latte")
